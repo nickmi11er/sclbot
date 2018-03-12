064c830b5d3fb2972dc5b2e2d993fb95abb76aa1
Nick Miller <nickmiller.on@gmail.com>
2018-03-12 23:29:18 +0300

    add user model

---
+++ src/models/model.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+import operator
+
+class Model(object):
+    saved_hash = 0
+
+    # Describe all attributes of class
+    # Note that the names of attributes MUST be the same as the names in DB
+    def __init__(self):
+        pass
+        
+
+    #Interface
+    #=========================================
+
+    # Create new entity with attributes 
+    # in args {name:value} by using reflection
+    @classmethod
+    def create(cls, args):
+        raise NotImplementedError('Method not implemented')
+
+    # Get entity from cache or DB by key
+    @classmethod
+    def get(cls, key):
+        entity = cls._get_from_cache(key)
+        if entity:
+            return entity
+        entity = cls._fetch_by_id(key)
+        if entity:
+            return entity
+        return entity
+
+    # Add or update entity in DB and/or cache
+    def save(self):
+        raise NotImplementedError('Method not implemented')
+
+    #=========================================
+
+
+    @staticmethod
+    def _get_from_cache(key):
+        raise NotImplementedError('Method not implemented')
+
+    @staticmethod
+    def _fetch_by_id(key):
+        raise NotImplementedError('Method not implemented')
+        
+
+    # Return hash summ of all self attributes with their values
+    # except saved_hash
+    def __hash__(self):
+        args = vars(self)
+        _key = ()
+        for k in sorted(args, key=args.get):
+            if k == 'saved_hash':
+                continue
+            _key = _key + (args[k],)
+        return hash(_key)
+
+
+    def __str__(self):
+        res = ''
+        args = vars(self)
+        for k in sorted(args, key=args.get):
+            val = args[k]
+            if isinstance(val, basestring):
+                val = val.encode('utf-8')
+            else:
+                val = str(val)
+            res = res + k + ': ' + val + '\n'
+        return res
+
+
+    # Map args values on object attributes by using reflection
+    # It is better way to describe all attributes in __init__
+    # to avoid to access nonexistent attributes in runtime
+    def _map(self, args):
+        for key in args:
+            self.__add_attr(key, args[key])
+
+    def __add_attr(self, name, value):
+        setattr(self, name, value)
---
+++ src/models/user.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+import sys
+sys.path.append('..')
+import data_manager as dm
+from cache_manager import Cache
+from model import Model
+
+cache = Cache()
+
+class User(Model):
+    
+    def __init__(self):
+        self.username = ''
+        self.tg_user_id = ''
+        self.role = 0
+        self.group_id = 0
+        self.group_name = ''
+
+
+    @classmethod
+    def create(cls, args):
+        user = User()
+        group_name = dm.get_group_by_id(args['group_id'])['group_name']
+        args['group_name'] = group_name
+        cls._map(user, args)
+        return user
+
+
+    def save(self):
+        if self.__hash__() == self.saved_hash:
+            return
+        self.saved_hash = self.__hash__()
+        dm.add_or_update_user(self.username.encode('utf-8'), self.tg_user_id, self.role, self.group_id)
+        cache.set(self.tg_user_id, self)
+
+
+    @staticmethod
+    def _get_from_cache(tg_user_id):
+        user = cache.get(tg_user_id)
+        if user:
+            return user
+        else:
+            return None
+
+
+    @staticmethod
+    def _fetch_by_id(tg_user_id):
+        user = User()
+        user_entity = dm.get_user(tg_user_id)
+        if user_entity:
+            user._map(user_entity)
+            cache.set(user.tg_user_id, user)
+            return user
+        else:
+            return None
+            
+
+
+
+
+    
