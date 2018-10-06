19483cf5fe0d6a612ab75e448e508b5e44663124
Nick Miller <nickmiller.on@gmail.com>
2018-10-06 13:20:54 +0300

    get rid of dead code

--- src/scl_manager.py
+++ src/scl_manager.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
-from openpyxl import load_workbook
 from datetime import datetime, timedelta
 import const
 import date_manager
 import re
-import subprocess
 from models.user import User
 import urllib2
 import json
@@ -19,8 +17,6 @@ class Task:
         self.time = time
 
 pt = re.compile(r'(?:(кр|)\s*([0-9]+(?:,\s*[0-9]+)*)+\s*(н|ч)+\s*)?\s*(.+)', re.UNICODE)
-wb2 = load_workbook(filename=const.assets_dir + '/scl.xlsx')
-ws2 = wb2.worksheets[0]
 
 start_dt = datetime.strptime('03.09.2018', '%d.%m.%Y')
 end_dt = datetime.strptime('31.05.2019', '%d.%m.%Y')
@@ -35,169 +31,9 @@ scl_time = {
     4: "14:40-16:10",
     5: "16:20-17:50",
     6: "18:00-19:30"
-}
-
-def updscl():
-    subprocess.call('sh ../assets/dlscl.sh', shell=True)
-    global wb2
-    global ws2
-    wb2 = load_workbook(filename=const.assets_dir + '/scl.xlsx')
-    ws2 = wb2.worksheets[0]
-
-
-# Возвращает следующий символ в алфавите для инкременты столбца таблицы
-# TODO добавить логику при привышении количества букв в алфавите
-def inc_col_name(col_name):
-    if col_name == 'Z':
-        return 'AA'
-    elif len(col_name) > 1:
-        return 'A' + inc_col_name(col_name[1])
-    else:
-        return chr(ord(col_name) + 1)
-
-
-# Разбивает строку на три составляющие: 
-# 1 группа - номера недель, в которых стоит предмет 
-# 3 группа - название самого предмета 
-# На основании переданного номера недели, возвращает название предмета
-def choose_task(entity, wleft):
-    entity = entity.encode('utf-8')
-    res = pt.match(entity)
-    if res and res.group(1) and res.group(2):
-        num_weeks = re.split(r',', res.group(2).replace(' ', ''))
-        for num in num_weeks:
-            if int(num) == int(wleft):
-                return None
-        return res.group(4)
-    elif res and res.group(2):
-        num_weeks = re.split(r',', res.group(2).replace(' ', ''))
-        for num in num_weeks:
-            if int(num) == int(wleft):
-                return res.group(4)
-    else:
-        return 'kostil'
-            
-
-def _get_scl(gp_nm, date):
-    date = date.replace(hour=0, minute=0, second=0, microsecond=0)
-    result = []
-    loaded_tasks = 0
-    wleft = ((date - start_dt).days + 1) / 7 + 1    # номер текущей недели
-
-    wdoffset = 3    # отступ по ячейкам
-    # если запрашиваемый день не понедельник, добавляем смещение
-    if date.weekday() != 0:
-        wdoffset = wdoffset + date.weekday() * 12
-
-    findall = False
-    # перебираем учебные группы 
-    for col in ws2.iter_cols(min_col=1):
-        if findall:
-            break
-        for cell in col:
-            cv = cell.value
-            if cv and int(cell.row) == 2:
-                if isinstance(cv, basestring):
-                    cv = cv.replace(' ', '')
-            # нашли нужную группу
-            if cv == gp_nm:
-                cc = cell.column
-                # парсим расписание на запрошенный день
-                # перебираем следующие 12 ячеек с шагом 2 с учетом четности недели
-                for i in range(wdoffset + 1, wdoffset + 13, 2):
-                    loaded_tasks += 1
-                    daynum = i
-                    if wleft % 2 == 0:
-                        daynum = daynum + 1
-
-                    val = ws2[cc + str(daynum)].value
-                    if val is None:
-                        continue
-                    
-                    # парсим время пары, тип занятий и аудиторию
-                    time = scl_time[loaded_tasks]
-
-                    cn = inc_col_name(cell.column)
-                    fullnm = cn + str(daynum)
-                    task_tp = ws2[fullnm].value
-                    # если тип занятия - практика, добавляем символ восклицательного знака
-                    if task_tp:
-                        if task_tp.encode('utf-8') == 'пр':
-                            task_tp = '❗'
-                        elif task_tp.encode('utf-8') == 'лр':
-                            task_tp = '👩‍🔬'
-                        else:
-                            task_tp = '▫️'
-                    else:
-                        task_tp = '▫️'
-
-                    cn = inc_col_name(cn)
-                    cn = inc_col_name(cn)
-                    fullnm = cn + str(daynum)
-                    cl_num = ws2[fullnm].value
-                    if cl_num:
-                        if isinstance(cl_num, long) or isinstance(cl_num, float):
-                            cl_num = str(int(cl_num))
-                        else:
-                            cl_num = cl_num.encode('utf-8')
-                        cl_num = '(' + cl_num + ')'
-                    else:
-                        cl_num = ''
-
-                    # проверяем существует ли вариативность пар на один и тот же промежуток времени
-                    ent = re.split(r'\n', val)
-                    if len(ent) == 1:
-                        res = choose_task(ent[0], wleft)
-                        if res is not None:
-                            if res == 'kostil':
-                                res = ent[0].encode('utf-8')
-                            result.append('{} {} {} {}'.format(task_tp, time, res, cl_num))
-                    # если существует вариативность, выбираем необходимый предмет
-                    elif len(ent) > 1:
-                        ch_flag = 0
-                        for i in ent:
-                            res = choose_task(i, wleft)
-                            if res is not None:
-                                if res == 'kostil':
-                                    continue
-                                cl_nums = re.split(r'\n', cl_num)
-                                if len(cl_nums) == 2:
-                                    cl_num = cl_nums[ch_flag]
-                                result.append('{} {} {} {}'.format(task_tp, time, res, cl_num))
-                                break
-                            ch_flag += 1
-                findall = True
-                break
-    return result     
+}  
         
 m_bold = lambda s: '*' + s + '*'
-
-def get_scl_with(dt, id):
-    date = date_manager.m_now()
-    user = User.get(id)
-    if dt is not None:
-        date = dt
-
-    if date < start_dt:
-        return u'Учеба еще не началась'
-
-    if date >= end_dt:
-        return u'Учеба закончилась. Удачи на сессии'
-
-    if date >= start_holy_dt:
-        return u'Летние каникулы. Отдыхай!'
-
-    out = m_bold('Расписание пар на {} ({}):'.format(date.strftime('%d.%m.%Y'), date_manager.rus_week_day[date.weekday()])) + '\n\n'
-    res = _get_scl(user.group_name, date)
-
-    if res:
-        for r in res:
-            out = out + r + "\n"
-    else:
-        out = out + "Пар нет. Отдыхай!\n"
-
-    return out
-
 
 def get_week_scl(dt, id):
     date = date_manager.m_now()
@@ -215,7 +51,7 @@ def get_week_scl(dt, id):
     for i in range(0, 6):
         if i > 0:
             out = out + '\n'
-        out = out + get_scl_with(date_manager.get_day_over(i, start), id)
+        out = out + get_scl(date_manager.get_day_over(i, start), id)
         
     return out
 
@@ -322,3 +158,16 @@ def get_scl(dt, id):
         out = out + "Пар нет. Отдыхай!\n"
 
     return out
+
+
+def institutes():
+    institutes = json.loads(urllib2.urlopen("http://localhost:9000/institutes").read())
+    return institutes
+
+def root_groups(inst):
+    root_gps = json.loads(urllib2.urlopen("http://localhost:9000/rootGroups?inst="+inst.encode('utf-8')).read())
+    return root_gps
+
+def groups(root_gp):
+    groups = json.loads(urllib2.urlopen("http://localhost:9000/groups?rootGroup="+root_gp.encode('utf-8')).read())
+    return groups
