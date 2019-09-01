49a8a308e051f82612b81f27294bcdaf24c60bfc
Nick Miller <nickmiller.on@gmail.com>
2019-09-01 10:49:22 +0300

    Fixed dates

--- src/scl_manager.py
+++ src/scl_manager.py
@@ -21,7 +21,7 @@ pt = re.compile(r'(?:(кр|)\s*([0-9]+(?:,|\s*[0-9]+)*)+\s*(н|ч)+\s*)?\s*(.+)'
 start_dt = datetime.strptime('01.09.2019', '%d.%m.%Y')
 
 end_dt = datetime.strptime('31.12.2019', '%d.%m.%Y')
-start_holy_dt = datetime.strptime('29.05.2019', '%d.%m.%Y')
+start_holy_dt = datetime.strptime('31.12.2019', '%d.%m.%Y')
 
 study_year = "2019"
 
@@ -106,8 +106,8 @@ def get_scl(dt, id):
     if date >= end_dt:
         return u'Учеба закончилась. Удачи на сессии'
 
-    if date >= start_holy_dt:
-        return u'Летние каникулы. Отдыхай!'
+    # if date >= start_holy_dt:
+    #     return u'Летние каникулы. Отдыхай!'
 
     dow = date.weekday()
     if dow == 6:
